c5542ea964b669283bef1993499710849e6aeed9
mrvishalsharma <mrvishalsharma8@gmail.com>
2021-07-21 12:46:42 +0530

    created and added home page

---
+++ script/script.js
@@ -0,0 +1,41 @@
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close')
+
+      // show
+  toggleMenu.addEventListener('click', ()=> {
+    navMenu.classList.toggle('show')
+  })
+
+  closeMenu.addEventListener('click', ()=> {
+    navMenu.classList.remove('show')
+  })
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction() {
+  navMenu.clasList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll',scrollActive)
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+      const sectionTop= current.offsetTop - 50
+      sectionId = current.getAttribute('id')
+
+      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        document.querySelector('.nav_menu a[href*='+ sectionId +']').clasList.add('active')
+      } else{
+          document.querySelector('.nav_menu a[href*='+ sectionId +']').clasList.remove('active')
+      }
+  })
+
+}
